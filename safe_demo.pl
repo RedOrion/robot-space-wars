f24368b52e0d75fc5c259d41d62f0044dd00c4c3
Iain C Docherty <Ian.Docherty1@nomura.com>
2013-11-13 14:16:22 +0000

    Changes to demo program

--- safe_demo.pl
+++ safe_demo.pl
@@ -2,40 +2,55 @@ use strict;
 use warnings;
 
 use Safe;
+use Data::Dumper;
 
 my $compartment = new Safe;
-#$compartment->permit(":default");
+$compartment->permit(":load",qw(pack sort print));
 
-my $a = {
-    foo => 'bar',
-    bar => {
-        fee => 2,
-    },
-};
+my $scratchpad = {};
 
-sub w_get {
-    return $a;
+sub w_get_scratchpad {
+    return $scratchpad;
 }
 
 sub w_print {
     my ($val) = @_;
     print $val;
 }
-$compartment->share('&w_print');
-$compartment->share('&w_get');
+sub w_Dumper {
+    my ($arg) = @_;
+    print "got here\n";
+    print Dumper(\$arg);
+}
+w_print(w_Dumper({foo => 1}));
+
+$compartment->share('w_print');
+$compartment->share('$scratchpad');
+$compartment->share('w_Dumper');
 
 my $code = <<'EOT';
-    my $a = w_get();
-    $a->{bar}{fee}++;
-    w_print $a->{bar}{fee}." ###\n";
-    #print("test");
+    use strict;
+    use warnings;
+    use Data::Dumper;
+
+    my $s = $scratchpad;
+    if (not $s->{init}) {
+        $s->{init} = 1;
+        $s->{counter} = 1;
+    }
+    else {
+        $s->{counter}++;
+    }
+    w_print "Counter: ".w_Dumper($s)."\n";
 EOT
 
-my $result = $compartment->reval($code);
-if ($@) {
-    print "ERROR: $@";
+for (my $i = 0; $i < 10; $i++) {
+    my $result = $compartment->reval($code);
+    if ($@) {
+        print "ERROR: $@";
+        exit;
+    }
 }
 
-
-print "result [$result][$a->{bar}{fee}]\n";
+print 'result: '.Dumper($scratchpad);
 1;
